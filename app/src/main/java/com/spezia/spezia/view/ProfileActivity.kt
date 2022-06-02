5663c83474b6d7bd0964bf5d18d8a8eaf373c4aa
M Fadhil Aulia <fadhilauliia@github.com>
2022-06-02 15:59:17 +0700

    Try to commit (Not yet for design of all)

---
+++ app/src/main/java/com/spezia/spezia/view/ProfileActivity.kt
@@ -0,0 +1,12 @@
+package com.spezia.spezia.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.spezia.spezia.R
+
+class ProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+    }
+}
---
+++ app/src/main/java/com/spezia/spezia/view/ScanActivity.kt
@@ -0,0 +1,12 @@
+package com.spezia.spezia.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.spezia.spezia.R
+
+class ScanActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_scan)
+    }
+}
